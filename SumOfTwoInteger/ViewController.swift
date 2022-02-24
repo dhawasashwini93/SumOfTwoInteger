7e37a77c9d89283bc1444a74273d5306e79e7829
ashwini <ashwini.d@healthmeterservices.in>
2022-02-24 15:42:17 +0530

    Add sum of two integer

--- SumOfTwoInteger/ViewController.swift
+++ SumOfTwoInteger/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var num1 : Int?
+    var num2 : Int?
+    var totalAmount : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        SumOfTwoInteger()
     }
 
+    //MARK: Sum of Two Integer
+    
+    func SumOfTwoInteger() {
+        num1 = 10
+        num2 = 20
+        totalAmount  = num1! + num2!
+        print("Total Amount \(String(describing: totalAmount!))")
+    }
 
 }
 
